24169313ff440b492e9acba04078fffb49e76c74
Jonathan Bauer <97413100+Jonathan-Bauer9001@users.noreply.github.com>
2025-10-20 00:28:03 -0500

    Chapter 9 Lab
    Auction app assignment completed

---
+++ Labs/CH09/Auctions/Auctions/Data/Services/ListingsService.cs
@@ -0,0 +1,42 @@
+﻿using Auctions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data.Services
+{
+    public class ListingsService : IListingsService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListingsService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Listing listing)
+        {
+            _context.Listings.Add(listing);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Listing> GetAll()
+        {
+            var applicationDbContext = _context.Listings.Include(l => l.IdentityUser);
+            return applicationDbContext;
+        }
+        public async Task<Listing> GetById(int? id)
+        {
+            var listing = await _context.Listings
+                .Include(l => l.IdentityUser)
+                .Include(l => l.Comments)
+                .Include(l => l.Bids)
+                .ThenInclude(b => b.IdentityUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            return listing;
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
